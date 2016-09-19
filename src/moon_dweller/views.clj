f0224780df05827a9c05d486d8ff9faa3b4ceed1
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-19 21:56:04 +1000

    Fixed HTML template

--- src/moon_dweller/views.clj
+++ src/moon_dweller/views.clj
@@ -3,18 +3,21 @@
 
 (defn core []
   (html
-    [:head 
-      [:title "Moon Dweller"]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]]
-    [:body
-      [:h1 "Moon Dweller"]
-      [:div {:id "title"}
-        [:img {:class "desktop" :src "/images/heading.png" :alt "Moon Dweller"}]]
-      [:ul {:id "history"}]
-      [:div {:id "block"}]
-      [:form {:id "commands"}
-        [:input {:type "text" :id "command" :autocomplete "off"}]]
-      [:a {:id "github-ribbon" :href "https://github.com/buntine/moondweller"}
-        [:img {:src "https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" :alt "Fork me on GitHub" :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"}]]]
+    [:html
+      [:head 
+        [:title "Moon Dweller"]
+        [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+        [:meta {:name "author" :content "Andrew Buntine"}]
+        [:meta {:name "description" :content "A text-based adventure. You've woken abruptly in a small, silver-walled room with no windows. There is a door to the east. What will you do?"}]]
+      [:body
+        [:h1 "Moon Dweller"]
+        [:div {:id "title"}
+          [:img {:class "desktop" :src "/images/heading.png" :alt "Moon Dweller"}]]
+        [:ul {:id "history"}]
+        [:div {:id "block"}]
+        [:form {:id "commands"}
+          [:input {:type "text" :id "command" :autocomplete "off"}]]
+        [:a {:id "github-ribbon" :href "https://github.com/buntine/moondweller"}
+          [:img {:src "https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" :alt "Fork me on GitHub" :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"}]]]]
      (include-css "/css/main.css")
      (include-js "/js/main.js")))
